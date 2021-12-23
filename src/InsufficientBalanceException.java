a86758a2cbd3daa23d5d5f38e6910a624b593f94
Sravani1518 <95915525+Sravani1518@users.noreply.github.com>
2021-12-23 21:52:56 +0530

    Add files via upload

---
+++ src/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+public class InsufficientBalanceException extends Exception {
+public InsufficientBalanceException()
+{
+super();
+
+}
+public InsufficientBalanceException(String msg)
+{
+super(msg);
+}
+
+
+
+}
